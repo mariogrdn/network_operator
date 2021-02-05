4c68b5b6b3663dbde1061d3cce19c4c12862b939
Mario Giordano <34249686+mariogrdn@users.noreply.github.com>
2021-02-05 13:03:39 +0100

    Update operator.go

--- operator.go
+++ operator.go
@@ -1,66 +1,82 @@
 package main
 
 import (
-    "fmt"
+	"bytes"
+	"fmt"
+	"net/http"
 	"os/exec"
-	"time"
 	"strconv"
 	"strings"
+	"time"
 )
 
-func main(){
+func main() {
 
 	changeTime := time.Time{}
 	state := "remote"
-	fmt.Printf(remoteInstance())
+	//fmt.Printf(remoteInstance())
 
 	for {
 		quality, err := strconv.Atoi(strings.TrimSuffix(getNetQuality(), "\n"))
-		if err != nil{
+		if err != nil {
 			fmt.Printf("Error during parsing. Check your internet connection\n")
-			time.Sleep(2000*time.Millisecond)
+			time.Sleep(2000 * time.Millisecond)
 			continue
 		}
 		strenght, err := strconv.Atoi(strings.TrimSuffix(getSigStrenght(), "\n"))
-		if err != nil{
+		if err != nil {
 			fmt.Printf("Error during parsing. Check your internet connection\n")
-			time.Sleep(2000*time.Millisecond)
+			time.Sleep(2000 * time.Millisecond)
 			continue
 		}
 		fmt.Printf("Quality: %d/100\nSignal: %d dB\n", quality, strenght)
 
-		if(quality <= 40 || strenght <= -60){
-			if (state == "local"){
-				fmt.Printf("Already using localInstance\n")
-			}else{
-				fmt.Printf("Switching to localInstance\n")
+		if quality <= 40 || strenght <= -60 {
+			if state == "local" {
+				fmt.Printf("Already using local instance\n")
+			} else {
+				fmt.Printf("Switching to local insatnce\n")
+
 				currentTime := time.Now()
-				if(currentTime.Sub(changeTime) <= 90000000000){
-				fmt.Printf("The last switching was too recent\n")
-				continue
+				if currentTime.Sub(changeTime) <= 90000000000 {
+					fmt.Printf("The last switching was too recent\n")
+					continue
 				}
-				state = "local"
-				changeTime = time.Now()
-				fmt.Printf(localInstance())
+
+				if selectorPatcher("local") == "Error" {
+					fmt.Printf("Error while switching instance")
+					continue
+				} else {
+					fmt.Printf("Switching executed successfully")
+					state = "local"
+					changeTime = time.Now()
+				}
+				//fmt.Printf(localInstance())
 			}
-		}else{			
-			if(state == "remote"){
-				fmt.Printf("Already using remoteInstance\n")
-			}else{
-				fmt.Printf("Switching to remoteInstance\n")
+		} else {
+			if state == "remote" {
+				fmt.Printf("Already using remote instance\n")
+			} else {
+				fmt.Printf("Switching to remote instance\n")
 				currentTime := time.Now()
-				if(currentTime.Sub(changeTime) <= 90000000000){
-				fmt.Printf("The last switching was too recent\n")
-				continue
+				if currentTime.Sub(changeTime) <= 90000000000 {
+					fmt.Printf("The last switching was too recent\n")
+					continue
 				}
-				state = "remote"
-				changeTime = time.Now()
-				fmt.Printf(remoteInstance())
+				if selectorPatcher("remote") == "Error" {
+					fmt.Printf("Error while switching instance")
+					continue
+				} else {
+					fmt.Printf("Switching executed successfully")
+					state = "remote"
+					changeTime = time.Now()
+				}
+				//fmt.Printf(remoteInstance())
 			}
 		}
 		time.Sleep(1000 * time.Millisecond)
 	}
-	
+
 }
 
 func getNetQuality() string {
@@ -81,10 +97,29 @@ func getSigStrenght() string {
 	return string(out)
 }
 
-func localInstance() string {
+/*/func localInstance() string {
 	return "Local instance\n"
 }
 
 func remoteInstance() string {
 	return "Remote instance\n"
+}*/
+
+func selectorPatcher(selector string) string {
+	baseURL := "http://127.0.0.1:8001/api/v1/namespaces/default/services/listener-service"
+	if selector == "local" || selector == "remote" {
+		ymlString := []byte("{\"spec\":{\"selector\":{\"app\":\"listener\",\"version\":\"" + selector + "\"}}}")
+		fmt.Println(string(ymlString))
+		req, _ := http.NewRequest(http.MethodPatch, baseURL, bytes.NewBuffer(ymlString))
+		req.Header.Set("Content-Type", "application/strategic-merge-patch+json")
+		_, err := http.DefaultClient.Do(req)
+		if err != nil {
+			fmt.Println("Error while doing the HTTP Request")
+			fmt.Println(err)
+			return "Error"
+		}
+	}
+
+	fmt.Println("Switch executed successfully")
+	return "Ok"
 }
